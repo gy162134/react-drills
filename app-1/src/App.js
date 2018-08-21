4e6c206c18295ba848bc93c798570af7aec3fe37
Garrett Yaworski <garrett@Garretts-MacBook-Pro.local>
2018-08-21 16:30:06 -0500

    App 5 done

--- app-1/src/App.js
+++ app-1/src/App.js
@@ -7,7 +7,7 @@ import InputBox from './InputBox.js'
 class App extends Component {
   render() {
     return (
-      <div>
+      <div className='App'>
         <InputBox/>
       </div>
     );
---
+++ app-5/src/Image.js
@@ -0,0 +1,5 @@
+import React from "react"
+
+const Image = () => <img src="https://bestwallpapers.co/wp-content/uploads/2018/05/black-background-pokemon-wallpapers-free-right-here-high-quality.jpg" alt=""></img>
+
+export default Image;
